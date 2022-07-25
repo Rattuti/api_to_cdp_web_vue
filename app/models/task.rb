7003567bc3366b2eaf48650e01d511548661d47c
Rattuti <i.ishiharar.96296@gmail.com>
2022-07-25 22:23:24 +0900

    init

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-    validates :title, presence: true
-    validates :description, presence: true
+validates :title, presence: true
+validates :description, presence: true
 end
